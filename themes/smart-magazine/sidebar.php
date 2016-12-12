f4e8f70fb3170612de762f53143b62ff765f83f5
105092 <u67463374@cbi-bs1.server.lan>
2016-12-12 09:44:19 -0500

    horizontal columns, image cleanup

--- themes/smart-magazine/sidebar.php
+++ themes/smart-magazine/sidebar.php
@@ -11,8 +11,6 @@ if (!is_active_sidebar('smart-magazine-sidebar-1')) {
 
 <div id="secondary" class="widget-area col-sm-4 col-md-4 sidebar container-fluid" role="complementary">
     <div class="row">
-        <div class="col-sm-2"></div>
-        <div class="col-sm-10"></div>
         <?php dynamic_sidebar('smart-magazine-sidebar-1'); ?>
     </div>
 </div><!-- #secondary -->
